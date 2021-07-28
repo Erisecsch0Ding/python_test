32bfb422749575fa917f157c8fb6824d930ec428
Erisecsch0Ding <88024667+Erisecsch0Ding@users.noreply.github.com>
2021-07-28 11:08:59 +0800

    Update and rename 15_Prime_num_judgement.py to 015_Prime_num_judgement.py

--- 015_Prime_num_judgement.py
+++ 015_Prime_num_judgement.py
@@ -1,4 +1,4 @@
-#filename : 15_Prime_num_judgement.py
+#filename : 015_Prime_num_judgement.py
 #author : erisecsch
 
 #质数判断
@@ -9,7 +9,7 @@ i = 0
 while True:
     i += 1
 
-    if i < 5: #循环5次
+    if i < 5: #循环5次,即让程序连续跑5遍
         num = int(input('请输入一个数字'))
         if num > 1:
             for i in range(2, num):  # 遍历因子
