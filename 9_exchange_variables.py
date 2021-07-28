22064b74fcdd30264ff3f2f2079056dc7b7c3c41
Erisecsch0Ding <88024667+Erisecsch0Ding@users.noreply.github.com>
2021-07-28 10:23:40 +0800

    Create 9_exchange_variables.py

---
+++ 9_exchange_variables.py
@@ -0,0 +1,17 @@
+#filename : 9_exchange_variables.py
+#author by : erisecsch
+
+#交换变量
+
+x = input("请输入x的值：")
+y = input("请输入y的值：")
+
+#创建临时变量temp
+temp = x
+
+temp = x
+x = y
+y = temp
+
+print("交换变量后的x值是：{}".format(x))
+print("交换变量后的y值是：{}".format(y))
