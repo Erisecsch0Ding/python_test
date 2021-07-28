79641612737c1ea0ab80f29a2c81a911775dbe5f
Erisecsch0Ding <88024667+Erisecsch0Ding@users.noreply.github.com>
2021-07-28 09:31:07 +0800

    Create 2_sum.py

---
+++ 2_sum.py
@@ -0,0 +1,9 @@
+#filename : 2_sum.py
+#author by : erisecsch
+
+#用户输入两个数字之后求和
+
+a = input("请输入第一个数字：")
+b = input("请输入第二个数字：")
+sum = float(a) + float(b)
+print("数字{0}跟数字{1}的和是：{2}".format(a,b,sum))
