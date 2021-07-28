69e256df9d6db4f871268fbfb07f20ebe24e574b
Erisecsch0Ding <88024667+Erisecsch0Ding@users.noreply.github.com>
2021-07-28 10:17:16 +0800

    Create 8_Celsius_to_Fahrenheit.py

---
+++ 8_Celsius_to_Fahrenheit.py
@@ -0,0 +1,8 @@
+#filename : 8_Celsius_to_Fahrenheit.py
+#author by : erisecsch
+
+#摄氏度转化华氏度
+
+celsius = float(input("输入摄氏度：")) #输入摄氏度
+fahrenheit = (celsius * 1.8) + 32
+print("{0:.1f} 摄氏度转化为华氏度为 {1:.1f}".format(clesius,fahrenheit))
